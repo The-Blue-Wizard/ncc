2104cf0667a0e7b4b0a6be9e53ff8644fb158e86
Charles Youse <charles@gnuless.org>
2019-01-20 14:16:51 -0500

    expand test cases
    add sieve of eratosthenes test case
    more thoroughly exercise test support routines

--- test/support.h
+++ test/support.h
@@ -1,3 +1,3 @@
 extern void putc(int);
 extern void puts(const char *);
-
+extern void putn(long);
--- test/support_c.c
+++ test/support_c.c
@@ -6,3 +6,26 @@ puts(const char *s)
     while (*s) putc(*s++);
     putc('\n');
 }
+
+void
+putn(long l)
+{
+    char buffer[20];    /* 2^64 = 20 decimal digits max */
+    int  i = 0;
+    unsigned long ul;
+
+    if (l < 0) {
+        putc('-');
+        l = -l;
+    }
+
+    ul = l;
+
+    do {
+        buffer[i++] = (ul % 10) + '0';
+        ul /= 10;
+    } while (ul);
+
+    while (i) putc(buffer[--i]);
+    putc('\n');
+}
